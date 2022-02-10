0368c40a381d14404aea7fc82966b98c634d1cc3
Terence O'Donoghue <->
2022-02-10 20:02:32 +1100

    Create useInterval hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+const useInterval = function useInterval(
+  callback: () => void,
+  delay: number,
+  active = true,
+) {
+  const callbackRef = useRef<() => void>();
+  const intervalRef = useRef<number>();
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (callbackRef.current && active) {
+      intervalRef.current = window.setInterval(callbackRef.current, delay);
+    }
+
+    return () => clearInterval(intervalRef.current);
+  }, [active, delay]);
+};
+
+export default useInterval;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as useEyeDropper } from './hooks/useEyeDropper';
+export { default as useInterval } from './hooks/useInterval';
 export { default as useLocalStorage } from './hooks/useLocalStorage';
 export { default as useOrientation } from './hooks/useOrientation';
 export { default as usePermission } from './hooks/usePermission';
