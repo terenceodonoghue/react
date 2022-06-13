5f42ebdfef15e69827e97493a7363a3d7ceeadf9
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-06-13 16:24:47 +1000

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:mdx/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'react/function-component-definition': [
-      'warn',
-      {
-        namedComponents: 'arrow-function',
-      },
-    ],
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 'off',
-    'react/require-default-props': 'off',
-  },
+  extends: ['@terenceodonoghue/react', 'plugin:mdx/recommended'],
   settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
     'mdx/code-blocks': true,
   },
 };
