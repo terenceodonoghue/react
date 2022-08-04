d142fb33d145558fd62e47016bf7412b02364f5a
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-08-04 15:57:13 +1000

    Add TextField to vehicles page

--- apps/velocity/pages/vehicles.tsx
+++ apps/velocity/pages/vehicles.tsx
@@ -1,30 +1,20 @@
 import { Interpolation, Theme } from '@emotion/react';
-import { Card } from '@terenceodonoghue/react-components/velocity';
+import { Card, TextField } from '@terenceodonoghue/react-components/velocity';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { em } from 'polished';
 import Container from '../components/core/Container';
 import Flex from '../components/core/Flex';
 import Slider from '../components/core/Slider';
-import TextField from '../components/core/TextField';
 
 const VehiclesPage: NextPage = () => {
-  const slider: Interpolation<Theme> = {
-    display: 'block',
-    marginBottom: 32,
-  };
-
-  const textField: Interpolation<Theme> = ({ palette, typography }) => ({
-    color: palette.neutral[900],
-    fontSize: em(15, typography.fontSize),
-    fontWeight: typography.fontWeight.regular,
+  const textField: Interpolation<Theme> = ({ palette }) => ({
     input: {
+      background: 'none',
       color: palette.accent,
-      fontSize: '1em',
+      '::placeholder': {
+        color: palette.neutral[600],
+      },
     },
-    letterSpacing: 'normal',
-    margin: '0 0 16px',
-    textTransform: 'none',
   });
 
   return (
@@ -35,38 +25,28 @@ const VehiclesPage: NextPage = () => {
       <Container heading="Vehicles Dashboard">
         <Flex>
           <div css={{ flex: 4 }} />
-          <Card css={{ flex: 1, paddingBottom: 8 }} heading="Filter">
-            <Slider css={slider} primary="Trips taken" secondary="753" />
-            <Slider css={slider} primary="Service due" secondary="14 days" />
-            <TextField
-              css={textField}
-              inputProps={{
-                placeholder: 'Vehicle model',
-                type: 'text',
-              }}
-            >
-              Vehicle model
-            </TextField>
-            <TextField
-              css={textField}
-              inputProps={{
-                defaultValue: 'Service needed',
-                placeholder: 'Status',
-                type: 'text',
-              }}
-            >
-              Status
-            </TextField>
-            <TextField
-              css={textField}
-              inputProps={{
-                defaultValue: 'USA',
-                placeholder: 'Location',
-                type: 'text',
-              }}
-            >
-              Location
-            </TextField>
+          <Card css={{ flex: 1 }} heading="Filter">
+            <Flex css={{ flexDirection: 'column', gap: 24 }}>
+              <Slider primary="Trips taken" secondary="753" />
+              <Slider primary="Service due" secondary="14 days" />
+              <TextField
+                css={textField}
+                label="Vehicle model"
+                placeholder="Vehicle model"
+              />
+              <TextField
+                css={textField}
+                defaultValue="Service needed"
+                label="Status"
+                placeholder="Status"
+              />
+              <TextField
+                css={textField}
+                defaultValue="USA"
+                label="Location"
+                placeholder="Location"
+              />
+            </Flex>
           </Card>
         </Flex>
       </Container>
