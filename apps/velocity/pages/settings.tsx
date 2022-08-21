fb57b9a3b89e61edecc4eb6002f3b58064ffaaea
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-08-21 18:57:11 +1000

    Remove unused import

--- apps/velocity/pages/settings.tsx
+++ apps/velocity/pages/settings.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { linearGradient, rgba } from 'polished';
+import { rgba } from 'polished';
 import { ComponentType, FunctionComponent, SVGProps } from 'react';
 
 import { Flex } from '@terenceodonoghue/react-components/core';
