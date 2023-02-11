5fec33a014c92fb614dac01cfd13eb74d8ae9229
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2023-02-11 16:36:32 +1100

    WIP

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['@terenceodonoghue/react', 'plugin:mdx/recommended'],
+  settings: {
+    'mdx/code-blocks': true,
+  },
+};
--- packages/react-components/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['**/__tests__/*'] },
-    ],
-  },
-};
