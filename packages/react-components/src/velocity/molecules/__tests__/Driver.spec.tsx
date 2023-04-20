620d3c5aaf33452473d42f0d0c88bc0b9a1791f5
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2023-04-20 18:33:37 +1000

    Update tests

--- packages/react-components/src/velocity/molecules/__tests__/Driver.spec.tsx
+++ packages/react-components/src/velocity/molecules/__tests__/Driver.spec.tsx
@@ -4,27 +4,29 @@ import ThemeProvider from '../../providers/ThemeProvider.js';
 import Driver from '../Driver.js';
 
 describe('Driver', () => {
-  it('has accessible components', () => {
-    // Arrange
-    const { screen } = render(
-      <Driver
-        name="Terence O'Donoghue"
-        totalDistance="1km"
-        totalEarnings="$1"
-        vehicle="Honda Civic"
-      />,
-      {
-        wrapper: ThemeProvider,
-      },
-    );
+  describe('has accessible components', () => {
+    test('with avatar', () => {
+      // Arrange
+      const { screen } = render(
+        <Driver
+          name="Terence O'Donoghue"
+          totalDistance="1km"
+          totalEarnings="$1"
+          vehicle="Honda Civic"
+        />,
+        {
+          wrapper: ThemeProvider,
+        },
+      );
 
-    // Assert
-    expect(screen.getByRole('img')).toHaveAccessibleName(
-      "Terence O'Donoghue's avatar",
-    );
+      // Assert
+      expect(screen.getByRole('img')).toHaveAccessibleName(
+        "Terence O'Donoghue's avatar",
+      );
+    });
   });
 
-  it('has visible components', () => {
+  it('has visible status', () => {
     // Arrange
     const { screen } = render(
       <Driver
@@ -41,6 +43,23 @@ describe('Driver', () => {
 
     // Assert
     expect(screen.getByRole('status')).toBeVisible();
+  });
+
+  it('has visible text', () => {
+    // Arrange
+    const { screen } = render(
+      <Driver
+        name="Terence O'Donoghue"
+        totalDistance="1km"
+        totalEarnings="$1"
+        vehicle="Honda Civic"
+      />,
+      {
+        wrapper: ThemeProvider,
+      },
+    );
+
+    // Assert
     expect(screen.getByText("Terence O'Donoghue")).toBeVisible();
     expect(screen.getByText('1km')).toBeVisible();
     expect(screen.getByText('$1')).toBeVisible();
--- packages/react-components/src/velocity/molecules/__tests__/Setting.spec.tsx
+++ packages/react-components/src/velocity/molecules/__tests__/Setting.spec.tsx
@@ -4,23 +4,25 @@ import ThemeProvider from '../../providers/ThemeProvider.js';
 import Setting from '../Setting.js';
 
 describe('Setting', () => {
-  it('has accessible components', () => {
-    // Arrange
-    const { screen } = render(
-      <Setting label="Label" description="Description" />,
-      {
-        wrapper: ThemeProvider,
-      },
-    );
+  describe('has accessible components', () => {
+    test('with switch', () => {
+      // Arrange
+      const { screen } = render(
+        <Setting label="Label" description="Description" />,
+        {
+          wrapper: ThemeProvider,
+        },
+      );
 
-    // Assert
-    expect(screen.getByRole('switch')).toHaveAccessibleName('Label');
-    expect(screen.getByRole('switch')).toHaveAccessibleDescription(
-      'Description',
-    );
+      // Assert
+      expect(screen.getByRole('switch')).toHaveAccessibleName('Label');
+      expect(screen.getByRole('switch')).toHaveAccessibleDescription(
+        'Description',
+      );
+    });
   });
 
-  it('has visible components', () => {
+  it('has visible text', () => {
     // Arrange
     const { screen } = render(
       <Setting label="Label" description="Description" />,
